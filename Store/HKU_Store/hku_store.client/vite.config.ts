90ecd8ab873d2455c9416851b6cecbe785e64fd1
HKU Project PC Account <Projectadm@NO64.hku.local>
2024-06-03 12:53:03 +0200

    http no s

--- Store/HKU_Store/hku_store.client/vite.config.ts
+++ Store/HKU_Store/hku_store.client/vite.config.ts
@@ -31,7 +31,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5037';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5037';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -47,7 +47,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/api/': {
-                target: 'https://localhost:5037',
+                target: 'http://localhost:5037',
                 changeOrigin: true,
                 secure: false
             }
